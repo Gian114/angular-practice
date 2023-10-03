f84127b772cf59f78476044cfb2cea7b9c7b4f0a
Gian114 <100781136+Gian114@users.noreply.github.com>
2023-10-03 16:59:21 +0200

    send data from parent to child and child to parent

--- practice-project/src/app/app.component.ts
+++ practice-project/src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'practice-project';
+
+  data = [ "ciao", "come", "stai"];
+
+  onReceive(data: string) {
+    console.log(data);
+  }
+  
 }
--- practice-project/src/app/slider/slider.component.ts
+++ practice-project/src/app/slider/slider.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-slider',
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./slider.component.css']
 })
 export class SliderComponent {
+
+  //prova passaggio dati da father a child
+  @Input() data: string[] = []; //con input si specifica che il dato arriverà da fuori
+
+  //passaggio da child a father
+  @Output() sendData = new EventEmitter<string>(); //con output si specifica che il dato uscirà da questo componente
+  figlio = 'ciao';
+
+  sendDataFunction() {
+    this.sendData.emit(this.figlio); 
+  }
+
+  //dai un'occhiata alle interfacce onInit, onChanges, ecc. per capire come funzionano
 
 }
